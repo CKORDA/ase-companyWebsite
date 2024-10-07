471db99a74e569e90c9c6eb712a15d099a95fef8
CKORDA <144379496+CKORDA@users.noreply.github.com>
2024-10-07 12:56:46 -0400

    Update Functions.php
    with other functions.php for readCSV

--- Functions.php
+++ Functions.php
@@ -5,3 +5,15 @@ function loadpage ($index){
     $content=file_get_contents($path);
     echo $content;
 }
+function readCSV($filename) {
+    $data = [];
+    if (($handle = fopen($filename, 'r')) !== false) {
+        while (($row = fgetcsv($handle, 1000, ',')) !== false) {
+            $data[] = $row;
+        }
+        fclose($handle);
+    }
+    return $data;
+}
+
+?>
