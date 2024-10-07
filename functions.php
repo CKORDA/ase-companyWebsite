110c6f4e0c5c679770dd6863bff3346404305a1b
tanakajimm1 <112527068+tanakajimm1@users.noreply.github.com>
2024-10-07 09:53:10 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,9 +2,12 @@
 function readCSV($filename) {
     $csvData = [];
     if (($handle = fopen($filename, "r")) !== FALSE) {
-        // Read each line of the CSV
+        // Read the header row
+        $header = fgetcsv($handle);
+        
+        // Read each line of the CSV and create an associative array
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            $csvData[] = $data; // Add each row to the array
+            $csvData[] = array_combine($header, $data); // Combine header with data
         }
         fclose($handle); // Close the file after reading
     }
