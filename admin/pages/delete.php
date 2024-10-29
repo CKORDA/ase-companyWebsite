5630b3eceb9167a0254f4c464ed9ee2da18da32b
MCQCrewman <123127669+MCQCrewman@users.noreply.github.com>
2024-10-28 20:04:24 -0400

    Update delete.php

--- admin/pages/delete.php
+++ admin/pages/delete.php
@@ -1,8 +1,10 @@
 <?php
 include_once 'pages.php';
 
+$pageManager = new PageManager();
+
 if (isset($_GET['index'])) {
-    deletePage($_GET['index']);
+    $pageManager->deletePage($_GET['index']);
     header("Location: index.php");
     exit;
 }
