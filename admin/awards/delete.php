d5d92fc84e1ead575f599cfcc934877d02a9a016
Rsignate <122956000+Rsignate@users.noreply.github.com>
2024-10-28 22:34:47 -0400

    delete.php

--- admin/awards/delete.php
+++ admin/awards/delete.php
@@ -1,31 +1,12 @@
 <?php
+require_once 'AwardManager.php'; // Make sure to include the class file
 
-function deleteAwardFromCSV($filename, $idToDelete) {
-    $awards = [];
-
-
-    if (($handle = fopen($filename, 'r')) !== false) {
-        $header = fgetcsv($handle); 
-        $awards[] = $header; 
-        while (($data = fgetcsv($handle)) !== false) {
-            if ($data[0] != $idToDelete) { 
-                $awards[] = $data; 
-            }
-        }
-        fclose($handle);
-    }
-
-    if (($handle = fopen($filename, 'w')) !== false) {
-        foreach ($awards as $award) {
-            fputcsv($handle, $award); 
-        }
-        fclose($handle);
-    }
-}
+$filename = 'data/awards.csv';
+$awardManager = new AwardManager($filename);
 
 if (isset($_GET['id'])) {
     $idToDelete = $_GET['id'];
-    deleteAwardFromCSV('data/awards.csv', $idToDelete);
+    $awardManager->deleteAward($idToDelete);
     header('Location: index.php'); 
     exit();
 } else {
